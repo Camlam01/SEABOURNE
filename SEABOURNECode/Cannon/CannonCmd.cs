5414cb65ff65642f54408c29bd4408683ef4d58a
Cameron James Lamb <Cameronlamb01@gmail.com>
2026-09-13 14:15:57 +0100

    Fix cannon command model import

--- SEABOURNECode/Cannon/CannonCmd.cs
+++ SEABOURNECode/Cannon/CannonCmd.cs
@@ -4,6 +4,7 @@ using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
 
 namespace SEABOURNE.SEABOURNECode.Cannon;
 
